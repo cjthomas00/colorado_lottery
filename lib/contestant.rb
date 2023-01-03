ed883e23eb934782be91cccfebb9454297ee38f8
Caleb Thomas <cjthomas00@comcast.net>
2023-01-02 17:28:06 -0700

    add initialize test and method.

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,10 +1,11 @@
 class Contestant
-  attr_reader :full_name, :age, :state_of_residence, :spending_money
+  attr_reader :full_name, :age, :state_of_residence, :spending_money, :game_interests
 
   def initialize(info)
     @full_name = "#{info[:first_name]} #{info[:last_name]}"
     @age = info[:age]
     @state_of_residence = info[:state_of_residence]
     @spending_money = info[:spending_money]
+    @game_interests = []
   end
 end
