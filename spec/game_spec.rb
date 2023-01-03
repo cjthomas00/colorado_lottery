25a864c0931cb5ec52fcce137a48f9c6fbc395b2
Caleb Thomas <cjthomas00@comcast.net>
2023-01-02 17:52:56 -0700

    refactor tests file to reduce lines.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,6 @@
-require './lib/contestant'
 require './lib/game'
 
 RSpec.describe Game do
-  let(:alexander) do 
-    Contestant.new({first_name: 'Alexander',
-                                      last_name: 'Aigiades',
-                                      age: 28,
-                                      state_of_residence: 'CO',
-                                      spending_money: 10})
-  end
   let(:mega_millions) { Game.new('Mega Millions', 5, true) }
   let(:pick_4) { Game.new('Pick 4', 2) }
 
